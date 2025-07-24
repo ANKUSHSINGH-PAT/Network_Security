bae55bffdf01f3f56b7bcdc0a819e927741da30c
Ankush <singhankush06@gmail.com>
2025-07-24 16:22:20 +0530

    etl pipeline completed

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,16 @@
+import os
+import logging
+from datetime import datetime
+
+
+LOG_FILE=f"{datetime.now().strftime('%Y-%m-%d')}.log"
+LOG_FILE_PATH = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+
+os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(message)s'
+)
+
