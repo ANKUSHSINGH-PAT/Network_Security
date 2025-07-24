5cc10c8d075333a1ac0aa64d497f43c23a3364ec
Ankush <singhankush06@gmail.com>
2025-07-24 11:22:57 +0530

    setup.py file ready

--- setup.py
+++ setup.py
@@ -1,1 +1,35 @@
+from setuptools import setup, find_packages, setup
+from typing import List
 
+
+requirement_list: List[str] = []
+
+filepath = 'requirements.txt'
+def read_requirements():
+
+    try:
+        """Read the requirements from a file and return a list of packages."""
+        with open(filepath, 'r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement!='-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print(f"Error: The file {filepath} was not found.")
+    return requirement_list
+    
+
+#print(read_requirements())
+
+setup(
+    name='Network_Security',
+    version='0.0.1',
+    author='Ankush Singh',
+    author_email="singhankush06@gmail.com",
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    description='A package for Network Security',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
